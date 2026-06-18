d7f6c14983778a5a44505df914b3c7dcc2b46b55
lostbean <talktoedgar@gmail.com>
2026-06-18 14:00:08 -0300

    chore: init

---
+++ lib/blau_drill.ex
@@ -0,0 +1,18 @@
+defmodule BlauDrill do
+  @moduledoc """
+  Documentation for `BlauDrill`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BlauDrill.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/blau_drill_test.exs
@@ -0,0 +1,8 @@
+defmodule BlauDrillTest do
+  use ExUnit.Case
+  doctest BlauDrill
+
+  test "greets the world" do
+    assert BlauDrill.hello() == :world
+  end
+end
